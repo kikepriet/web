9398a44c03581d2dc7db4f0feec6d859e45bc957
kike priet <kikepriet@gmail.com>
2019-02-24 14:02:43 -0600

    empezando con el website para mi sitio web personal

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/animate.css">
+
+    <title>kikepriet website</title>
+  </head>
+  <body>
+    <div class="imagen_index d-flex justify-content-center">
+        <div class="d-flex align-items-end">
+            <img class="img-fluid wow bounceInDown" src="images/logo_index.png" alt="">
+        </div>
+    </div>
+    <div class="imagen_index d-flex justify-content-center">
+        <ul class="nav mt-5 wow fadeInLeft" data-wow-delay="2s">
+            <li class="nav-item  text_index">
+                <a href="#" class="nav-link">¿Quién soy?</a>
+            </li>
+            <li class="nav-item  text_index">
+                <a href="#" class="nav-link">¿Qué hago?</a>
+            </li>
+            <li class="nav-item  text_index">
+                <a href="#" class="nav-link">¡Contáctame!</a>
+            </li>
+        </ul>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/wow.min.js"></script>
+              <script>
+              new WOW().init();
+              </script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
